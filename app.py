85eb78a532f1993d51a33f5a45cb13043f8d3dd1
viriya27447 <viriya27447@gmail.com>
2024-10-07 16:48:39 +0700

    init

--- app.py
+++ app.py
@@ -127,3 +127,9 @@ table_data = [["A1", "A2", "A3"],
               ["B1", "B2", "B3"],
               ["C1", "C2", "C3"]]
 st.table(table_data)  # Display the table
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
