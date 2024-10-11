753459cbd40c03bc54d52193c3d989261bacb0f9
viriya27447 <viriya27447@gmail.com>
2024-10-12 01:42:13 +0700

    init

--- 1210.py
+++ 1210.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-def page2():
-    st.title("Second page")
+def page1():
+    st.write(st.session_state.foo)
 
-pg = st.navigation([
-    st.Page("page1.py", title="First page", icon="🔥"),
-    st.Page(page2, title="Second page", icon=":material/favorite:"),
-])
+def page2():
+    st.write(st.session_state.bar)
+
+pg = st.navigation([st.Page(page1), st.Page(page2)])
 pg.run()
