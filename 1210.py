35bfdbc79d78dc1165706dd90c3df28b22654240
viriya27447 <viriya27447@gmail.com>
2024-10-12 01:52:31 +0700

    init

--- 1210.py
+++ 1210.py
@@ -1,20 +1,10 @@
 import streamlit as st
 
-# สร้างฟังก์ชันสำหรับแต่ละหน้า
-def page_one():
-    st.title("หน้า 1")
-    st.write("ยินดีต้อนรับสู่หน้า 1!")
+def page1():
+    st.write(st.session_state.foo)
 
-def page_two():
-    st.title("หน้า 2")
-    st.write("ยินดีต้อนรับสู่หน้า 2!")
+def page2():
+    st.write(st.session_state.bar)
 
-# เมนูด้านข้าง
-st.sidebar.title("เมนู")
-page = st.sidebar.radio("เลือกหน้า:", ("หน้า 1", "หน้า 2"))
-
-# แสดงหน้าตามที่เลือก
-if page == "หน้า 1":
-    page_one()
-else:
-    page_two()
+pg = st.navigation([st.Page(page1), st.Page(page2)])
+pg.run()
